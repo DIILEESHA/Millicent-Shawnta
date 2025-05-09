84065f99a96ea662a879609d9e8aed829a6de283
DIILEESHA <dileeshanawarathna18@gmail.com>
2025-05-09 11:54:05 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <Router>
       <div>
-        <Nav />
+        {/* <Nav /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/gallery" element={<Gallery />} />
